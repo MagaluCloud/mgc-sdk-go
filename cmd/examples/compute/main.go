574ed655e408cc3a84c9ed1f06ff1f7cc9c9a757
Felipe Publio <felipe@publio.dev>
2025-02-12 16:08:13 -0300

    feat: enhance instance tests with parallel execution and update example functions for renaming and retyping instances

--- cmd/examples/compute/main.go
+++ cmd/examples/compute/main.go
@@ -16,8 +16,36 @@ func main() {
 	ExampleListImages()
 	ExampleListInstances()
 	id := ExampleCreateInstance()
-	ExampleManageInstance(id)
+	ExampleGetInstance(id)
+	ExampleRenameAndRetypeInstance(id)
 	ExampleDeleteInstance(id)
+}
+
+func ExampleRenameAndRetypeInstance(id string) {
+	// Create a new client
+	apiToken := os.Getenv("MGC_API_TOKEN")
+	if apiToken == "" {
+		log.Fatal("MGC_API_TOKEN environment variable is not set")
+	}
+	c := client.NewMgcClient(apiToken)
+	computeClient := compute.New(c)
+	ctx := context.Background()
+	// Rename the instance
+	if err := computeClient.Instances().Rename(ctx, id, "new-name"); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Instance renamed successfully")
+
+	// Change machine type
+	retypeReq := compute.RetypeRequest{
+		MachineType: compute.IDOrName{
+			Name: helpers.StrPtr("BV2-2-20"),
+		},
+	}
+	if err := computeClient.Instances().Retype(ctx, id, retypeReq); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Instance machine type changed successfully")
 }
 
 func ExampleListInstances() {
@@ -26,14 +54,14 @@ func ExampleListInstances() {
 	if apiToken == "" {
 		log.Fatal("MGC_API_TOKEN environment variable is not set")
 	}
-	c := client.NewMgcClient(apiToken, client.WithBaseURL("https://api.magalu.cloud"))
+	c := client.NewMgcClient(apiToken)
 	computeClient := compute.New(c)
 
 	// List instances with pagination and sorting
 	instances, err := computeClient.Instances().List(context.Background(), compute.ListOptions{
 		Limit:  helpers.IntPtr(10),
 		Offset: helpers.IntPtr(0),
-		Expand: []string{compute.InstanceMachineTypeExpand, compute.InstanceImageExpand},
+		Expand: []string{compute.InstanceMachineTypeExpand, compute.InstanceImageExpand, compute.InstanceNetworkExpand},
 	})
 
 	if err != nil {
@@ -43,11 +71,26 @@ func ExampleListInstances() {
 	// Print instance details
 	for _, instance := range instances {
 		fmt.Printf("Instance: %s (ID: %s)\n", instance.Name, instance.ID)
-		fmt.Printf("  Machine Type: %s\n", *instance.MachineType.Name)
-		fmt.Printf("  Image: %s\n", *instance.Image.Name)
+		fmt.Printf("  Machine Type: %s\n", instance.MachineType.Name)
+		fmt.Printf("  Image: %s\n", instance.Image.Name)
 		fmt.Printf("  Status: %s\n", instance.Status)
 		fmt.Printf("  State: %s\n", instance.State)
 		fmt.Printf("  Created At: %s\n", instance.CreatedAt)
+		fmt.Printf("  Updated At: %s\n", instance.UpdatedAt)
+		fmt.Printf("  VPC ID: %s\n", *instance.Network.Vpc.ID)
+		fmt.Printf("  VPC Name: %s\n", *instance.Network.Vpc.Name)
+		for _, ni := range instance.Network.Interfaces {
+			fmt.Println("  Interface ID: ", ni.ID)
+			fmt.Println("  Interface Name: ", ni.Name)
+			fmt.Println("  Interface IPv4: ", ni.AssociatedPublicIpv4)
+			fmt.Println("  Interface IPv6: ", ni.IpAddresses.PublicIpv6)
+			fmt.Println("  Interface Local IPv4: ", ni.IpAddresses.PrivateIpv4)
+			fmt.Println("Is Primary: ", ni.Primary)
+			for _, sg := range ni.SecurityGroups {
+				fmt.Println("  Security Group ID: ", sg)
+			}
+			fmt.Println("--------")
+		}
 	}
 }
 
@@ -84,7 +127,7 @@ func ExampleCreateInstance() string {
 	return id
 }
 
-func ExampleManageInstance(id string) {
+func ExampleGetInstance(id string) {
 	apiToken := os.Getenv("MGC_API_TOKEN")
 	if apiToken == "" {
 		log.Fatal("MGC_API_TOKEN environment variable is not set")
@@ -100,23 +143,25 @@ func ExampleManageInstance(id string) {
 	}
 
 	fmt.Printf("Instance: %s (ID: %s)\n", instance.Name, instance.ID)
-
-	// Rename the instance
-	if err := computeClient.Instances().Rename(ctx, instance.ID, "new-name"); err != nil {
-		log.Fatal(err)
+	fmt.Printf("  Machine Type: %s\n", instance.MachineType.Name)
+	fmt.Printf("  Image: %s\n", instance.Image.Name)
+	fmt.Printf("  Status: %s\n", instance.Status)
+	fmt.Printf("  State: %s\n", instance.State)
+	fmt.Printf("  Created At: %s\n", instance.CreatedAt)
+	fmt.Printf("  Updated At: %s\n", instance.UpdatedAt)
+	fmt.Printf("  VPC ID: %s\n", *instance.Network.Vpc.ID)
+	fmt.Printf("  VPC Name: %s\n", *instance.Network.Vpc.Name)
+	for _, ni := range instance.Network.Interfaces {
+		fmt.Println("  Interface ID: ", ni.ID)
+		fmt.Println("  Interface Name: ", ni.Name)
+		fmt.Println("  Interface IPv4: ", ni.AssociatedPublicIpv4)
+		fmt.Println("  Interface IPv6: ", ni.IpAddresses.PublicIpv6)
+		fmt.Println("  Interface Local IPv4: ", ni.IpAddresses.PrivateIpv4)
+		fmt.Println("Is Primary: ", ni.Primary)
+		for _, sg := range ni.SecurityGroups {
+			fmt.Println("  Security Group ID: ", sg)
+		}
 	}
-	fmt.Println("Instance renamed successfully")
-
-	// Change machine type
-	retypeReq := compute.RetypeRequest{
-		MachineType: compute.IDOrName{
-			Name: helpers.StrPtr("BV2-2-20"),
-		},
-	}
-	if err := computeClient.Instances().Retype(ctx, instance.ID, retypeReq); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Instance machine type changed successfully")
 }
 
 func ExampleDeleteInstance(id string) {
