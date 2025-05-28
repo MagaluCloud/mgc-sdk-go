286b89bd8bba09942406c22cd7addd9d8560ed18
Gefferson Ferraz <me@gefferson.com.br>
2025-05-27 23:00:19 -0300

    feat: add loadbalancer listners

--- lbaas/network_listners.go
+++ lbaas/network_listners.go
@@ -1,19 +1,67 @@
 package lbaas
 
-import "context"
+import (
+	"context"
+	"strconv"
+
+	mgc_http "github.com/MagaluCloud/mgc-sdk-go/internal/http"
+)
 
 type (
-	CreateNetworkListenerRequest struct{}
-	DeleteNetworkListenerRequest struct{}
-	GetNetworkListenerRequest    struct{}
-	ListNetworkListenerRequest   struct{}
-	UpdateNetworkListenerRequest struct{}
+	CreateNetworkListenerRequest struct {
+		LoadBalancerID   string  `json:"-"`
+		BackendID        string  `json:"-"`
+		TLSCertificateID *string `json:"tls_certificate_id,omitempty"`
+		Name             string  `json:"name"`
+		Description      *string `json:"description,omitempty"`
+		Protocol         string  `json:"protocol"`
+		Port             int     `json:"port"`
+	}
+
+	DeleteNetworkListenerRequest struct {
+		LoadBalancerID string `json:"-"`
+		ListenerID     string `json:"-"`
+	}
+
+	GetNetworkListenerRequest struct {
+		LoadBalancerID string `json:"-"`
+		ListenerID     string `json:"-"`
+	}
+
+	ListNetworkListenerRequest struct {
+		LoadBalancerID string  `json:"-"`
+		Offset         *int    `json:"-"`
+		Limit          *int    `json:"-"`
+		Sort           *string `json:"-"`
+	}
+
+	UpdateNetworkListenerRequest struct {
+		LoadBalancerID   string  `json:"-"`
+		ListenerID       string  `json:"-"`
+		TLSCertificateID *string `json:"tls_certificate_id,omitempty"`
+	}
+
+	NetworkListenerResponse struct {
+		ID               string  `json:"id"`
+		TLSCertificateID *string `json:"tls_certificate_id,omitempty"`
+		BackendID        string  `json:"backend_id"`
+		Name             string  `json:"name"`
+		Description      *string `json:"description,omitempty"`
+		Protocol         string  `json:"protocol"`
+		Port             int     `json:"port"`
+		CreatedAt        string  `json:"created_at"`
+		UpdatedAt        string  `json:"updated_at"`
+	}
+
+	NetworkPaginatedListenerResponse struct {
+		Results []NetworkListenerResponse `json:"results"`
+	}
 
 	NetworkListenerService interface {
-		Create(ctx context.Context, req CreateNetworkListenerRequest) (string, error)
+		Create(ctx context.Context, req CreateNetworkListenerRequest) (*NetworkListenerResponse, error)
 		Delete(ctx context.Context, req DeleteNetworkListenerRequest) error
-		Get(ctx context.Context, req GetNetworkListenerRequest) (*any, error)
-		List(ctx context.Context, req ListNetworkListenerRequest) ([]any, error)
+		Get(ctx context.Context, req GetNetworkListenerRequest) (*NetworkListenerResponse, error)
+		List(ctx context.Context, req ListNetworkListenerRequest) ([]NetworkListenerResponse, error)
 		Update(ctx context.Context, req UpdateNetworkListenerRequest) error
 	}
 
@@ -22,27 +70,92 @@ type (
 	}
 )
 
-func (s *networkListenerService) Create(ctx context.Context, req CreateNetworkListenerRequest) (string, error) {
-	// POST /v0beta1/network-load-balancers/{load_balancer_id}/listeners
-	panic("not implemented")
+func (s *networkListenerService) Create(ctx context.Context, req CreateNetworkListenerRequest) (*NetworkListenerResponse, error) {
+	path := "/v0beta1/network-load-balancers/" + req.LoadBalancerID + "/listeners"
+
+	httpReq, err := s.client.newRequest(ctx, "POST", path, req)
+	if err != nil {
+		return nil, err
+	}
+
+	// Adicionar backend_id como query parameter obrigatório
+	query := httpReq.URL.Query()
+	query.Set("backend_id", req.BackendID)
+	httpReq.URL.RawQuery = query.Encode()
+
+	var resp NetworkListenerResponse
+	result, err := mgc_http.Do(s.client.GetConfig(), ctx, httpReq, &resp)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
 
 func (s *networkListenerService) Delete(ctx context.Context, req DeleteNetworkListenerRequest) error {
-	// DELETE /v0beta1/network-load-balancers/{load_balancer_id}/listeners/{listener_id}
-	panic("not implemented")
+	path := "/v0beta1/network-load-balancers/" + req.LoadBalancerID + "/listeners/" + req.ListenerID
+
+	httpReq, err := s.client.newRequest(ctx, "DELETE", path, nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = mgc_http.Do[any](s.client.GetConfig(), ctx, httpReq, nil)
+	return err
 }
 
-func (s *networkListenerService) Get(ctx context.Context, req GetNetworkListenerRequest) (*any, error) {
-	// GET /v0beta1/network-load-balancers/{load_balancer_id}/listeners/{listener_id}
-	panic("not implemented")
+func (s *networkListenerService) Get(ctx context.Context, req GetNetworkListenerRequest) (*NetworkListenerResponse, error) {
+	path := "/v0beta1/network-load-balancers/" + req.LoadBalancerID + "/listeners/" + req.ListenerID
+
+	httpReq, err := s.client.newRequest(ctx, "GET", path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var resp NetworkListenerResponse
+	result, err := mgc_http.Do(s.client.GetConfig(), ctx, httpReq, &resp)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
 
-func (s *networkListenerService) List(ctx context.Context, req ListNetworkListenerRequest) ([]any, error) {
-	// GET /v0beta1/network-load-balancers/{load_balancer_id}/listeners
-	panic("not implemented")
+func (s *networkListenerService) List(ctx context.Context, req ListNetworkListenerRequest) ([]NetworkListenerResponse, error) {
+	path := "/v0beta1/network-load-balancers/" + req.LoadBalancerID + "/listeners"
+
+	httpReq, err := s.client.newRequest(ctx, "GET", path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Adicionar query parameters se fornecidos
+	query := httpReq.URL.Query()
+	if req.Offset != nil {
+		query.Set("_offset", strconv.Itoa(*req.Offset))
+	}
+	if req.Limit != nil {
+		query.Set("_limit", strconv.Itoa(*req.Limit))
+	}
+	if req.Sort != nil {
+		query.Set("_sort", *req.Sort)
+	}
+	httpReq.URL.RawQuery = query.Encode()
+
+	var resp NetworkPaginatedListenerResponse
+	result, err := mgc_http.Do(s.client.GetConfig(), ctx, httpReq, &resp)
+	if err != nil {
+		return nil, err
+	}
+	return result.Results, nil
 }
 
 func (s *networkListenerService) Update(ctx context.Context, req UpdateNetworkListenerRequest) error {
-	// PUT /v0beta1/network-load-balancers/{load_balancer_id}/listeners/{listener_id}
-	panic("not implemented")
+	path := "/v0beta1/network-load-balancers/" + req.LoadBalancerID + "/listeners/" + req.ListenerID
+
+	httpReq, err := s.client.newRequest(ctx, "PUT", path, req)
+	if err != nil {
+		return err
+	}
+
+	_, err = mgc_http.Do[any](s.client.GetConfig(), ctx, httpReq, nil)
+	return err
 }
