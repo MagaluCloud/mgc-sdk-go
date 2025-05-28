8f216c9fb7d751ef9655c8e3d5503971f428fd22
Gefferson Ferraz <me@gefferson.com.br>
2025-05-27 22:44:40 -0300

    feat: add loadbalancer certificates

--- lbaas/network_certificates.go
+++ lbaas/network_certificates.go
@@ -1,19 +1,63 @@
 package lbaas
 
-import "context"
+import (
+	"context"
+	"strconv"
+
+	mgc_http "github.com/MagaluCloud/mgc-sdk-go/internal/http"
+)
 
 type (
-	CreateNetworkCertificateRequest struct{}
-	DeleteNetworkCertificateRequest struct{}
-	GetNetworkCertificateRequest    struct{}
-	ListNetworkCertificateRequest   struct{}
-	UpdateNetworkCertificateRequest struct{}
+	CreateNetworkCertificateRequest struct {
+		LoadBalancerID string  `json:"-"`
+		Name           string  `json:"name"`
+		Description    *string `json:"description,omitempty"`
+		Certificate    string  `json:"certificate"`
+		PrivateKey     string  `json:"private_key"`
+	}
+
+	DeleteNetworkCertificateRequest struct {
+		LoadBalancerID   string `json:"-"`
+		TLSCertificateID string `json:"-"`
+	}
+
+	GetNetworkCertificateRequest struct {
+		LoadBalancerID   string `json:"-"`
+		TLSCertificateID string `json:"-"`
+	}
+
+	ListNetworkCertificateRequest struct {
+		LoadBalancerID string  `json:"-"`
+		Offset         *int    `json:"-"`
+		Limit          *int    `json:"-"`
+		Sort           *string `json:"-"`
+	}
+
+	UpdateNetworkCertificateRequest struct {
+		LoadBalancerID   string `json:"-"`
+		TLSCertificateID string `json:"-"`
+		Certificate      string `json:"certificate"`
+		PrivateKey       string `json:"private_key"`
+	}
+
+	NetworkTLSCertificateResponse struct {
+		ID             string  `json:"id"`
+		Name           string  `json:"name"`
+		ExpirationDate *string `json:"expiration_date,omitempty"`
+		Description    *string `json:"description,omitempty"`
+		CreatedAt      string  `json:"created_at"`
+		UpdatedAt      string  `json:"updated_at"`
+	}
+
+	NetworkPaginatedTLSCertificateResponse struct {
+		Results []NetworkTLSCertificateResponse `json:"results"`
+	}
 
 	NetworkCertificateService interface {
-		Create(ctx context.Context, req CreateNetworkCertificateRequest) (string, error)
+		Create(ctx context.Context, req CreateNetworkCertificateRequest) (*NetworkTLSCertificateResponse, error)
 		Delete(ctx context.Context, req DeleteNetworkCertificateRequest) error
-		Get(ctx context.Context, req GetNetworkCertificateRequest) (*any, error)
-		List(ctx context.Context, req ListNetworkCertificateRequest) ([]any, error)
+		Get(ctx context.Context, req GetNetworkCertificateRequest) (*NetworkTLSCertificateResponse, error)
+		List(ctx context.Context, req ListNetworkCertificateRequest) ([]NetworkTLSCertificateResponse, error)
 		Update(ctx context.Context, req UpdateNetworkCertificateRequest) error
 	}
 
@@ -22,27 +66,87 @@ type (
 	}
 )
 
-func (s *networkCertificateService) Create(ctx context.Context, req CreateNetworkCertificateRequest) (string, error) {
-	// POST /v0beta1/network-load-balancers/{load_balancer_id}/tls-certificates
-	panic("not implemented")
+func (s *networkCertificateService) Create(ctx context.Context, req CreateNetworkCertificateRequest) (*NetworkTLSCertificateResponse, error) {
+	path := "/v0beta1/network-load-balancers/" + req.LoadBalancerID + "/tls-certificates"
+
+	httpReq, err := s.client.newRequest(ctx, "POST", path, req)
+	if err != nil {
+		return nil, err
+	}
+
+	var resp NetworkTLSCertificateResponse
+	result, err := mgc_http.Do(s.client.GetConfig(), ctx, httpReq, &resp)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
 
 func (s *networkCertificateService) Delete(ctx context.Context, req DeleteNetworkCertificateRequest) error {
-	// DELETE /v0beta1/network-load-balancers/{load_balancer_id}/tls-certificates/{tls_certificate_id}
-	panic("not implemented")
+	path := "/v0beta1/network-load-balancers/" + req.LoadBalancerID + "/tls-certificates/" + req.TLSCertificateID
+
+	httpReq, err := s.client.newRequest(ctx, "DELETE", path, nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = mgc_http.Do[any](s.client.GetConfig(), ctx, httpReq, nil)
+	return err
 }
 
-func (s *networkCertificateService) Get(ctx context.Context, req GetNetworkCertificateRequest) (*any, error) {
-	// GET /v0beta1/network-load-balancers/{load_balancer_id}/tls-certificates/{tls_certificate_id}
-	panic("not implemented")
+func (s *networkCertificateService) Get(ctx context.Context, req GetNetworkCertificateRequest) (*NetworkTLSCertificateResponse, error) {
+	path := "/v0beta1/network-load-balancers/" + req.LoadBalancerID + "/tls-certificates/" + req.TLSCertificateID
+
+	httpReq, err := s.client.newRequest(ctx, "GET", path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var resp NetworkTLSCertificateResponse
+	result, err := mgc_http.Do(s.client.GetConfig(), ctx, httpReq, &resp)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
 
-func (s *networkCertificateService) List(ctx context.Context, req ListNetworkCertificateRequest) ([]any, error) {
-	// GET /v0beta1/network-load-balancers/{load_balancer_id}/tls-certificates
-	panic("not implemented")
+func (s *networkCertificateService) List(ctx context.Context, req ListNetworkCertificateRequest) ([]NetworkTLSCertificateResponse, error) {
+	path := "/v0beta1/network-load-balancers/" + req.LoadBalancerID + "/tls-certificates"
+
+	httpReq, err := s.client.newRequest(ctx, "GET", path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Adicionar query parameters se fornecidos
+	query := httpReq.URL.Query()
+	if req.Offset != nil {
+		query.Set("_offset", strconv.Itoa(*req.Offset))
+	}
+	if req.Limit != nil {
+		query.Set("_limit", strconv.Itoa(*req.Limit))
+	}
+	if req.Sort != nil {
+		query.Set("_sort", *req.Sort)
+	}
+	httpReq.URL.RawQuery = query.Encode()
+
+	var resp NetworkPaginatedTLSCertificateResponse
+	result, err := mgc_http.Do(s.client.GetConfig(), ctx, httpReq, &resp)
+	if err != nil {
+		return nil, err
+	}
+	return result.Results, nil
 }
 
 func (s *networkCertificateService) Update(ctx context.Context, req UpdateNetworkCertificateRequest) error {
-	// PUT /v0beta1/network-load-balancers/{load_balancer_id}/tls-certificates/{tls_certificate_id}
-	panic("not implemented")
+	path := "/v0beta1/network-load-balancers/" + req.LoadBalancerID + "/tls-certificates/" + req.TLSCertificateID
+
+	httpReq, err := s.client.newRequest(ctx, "PUT", path, req)
+	if err != nil {
+		return err
+	}
+
+	_, err = mgc_http.Do[any](s.client.GetConfig(), ctx, httpReq, nil)
+	return err
 }
