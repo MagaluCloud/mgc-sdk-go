6715d607d7e260872521389edc11c1546c846efd
NathalyaStefhany <nathalya.stefhany@hotmail.com>
2025-11-27 16:49:50 -0300

    feat: implemented proxy cache examples

--- cmd/examples/containerregistry/main.go
+++ cmd/examples/containerregistry/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"log/slog"
@@ -37,6 +38,16 @@ func main() {
 		deleteRepository(c, id, name)
 	*/
 	deleteRegistry(c, id)
+
+	// Create Container Registry client
+	containerRegistryClient := containerregistry.New(c)
+
+	id = createProxyCache(containerRegistryClient)
+	listProxyCaches(containerRegistryClient)
+	listAllProxyCaches(containerRegistryClient)
+	updateProxyCache(containerRegistryClient, id)
+	getProxyCache(containerRegistryClient, id)
+	deleteProxyCache(containerRegistryClient, id)
 }
 
 func getCredentials(c *client.CoreClient) {
@@ -155,4 +166,162 @@ func getImages(c *client.CoreClient, id string, name string) {
 	for _, image := range images.Results {
 		fmt.Println("Image: ", image.Digest, "SizeBytes: ", image.SizeBytes, "PushedAt: ", image.PushedAt, "PulledAt: ", image.PulledAt, "ManifestMediaType: ", image.ManifestMediaType, "MediaType: ", image.MediaType, "Tags: ", image.Tags, "TagsDetails: ", image.TagsDetails, "ExtraAttr: ", image.ExtraAttr)
 	}
+}
+
+func listProxyCaches(crClient *containerregistry.ContainerRegistryClient) {
+	// List proxy caches with pagination
+	resp, err := crClient.ProxyCaches().List(context.Background(), containerregistry.ProxyCacheListOptions{
+		Limit:  helpers.IntPtr(10),
+		Offset: helpers.IntPtr(0),
+		Sort:   helpers.StrPtr("name:desc"),
+	})
+
+	if err != nil {
+		var httpError *client.HTTPError
+		if errors.As(err, &httpError) {
+			fmt.Printf("Status %s\n", httpError.Status)
+			fmt.Printf("Error body: %s\n", string(httpError.Body))
+		}
+
+		log.Fatal("Failed to list proxy-caches")
+	}
+
+	// Print pagination metadata
+	fmt.Printf("Proxy-caches (Page %d-%d of %d total)\n",
+		resp.Meta.Page.Offset,
+		resp.Meta.Page.Offset+resp.Meta.Page.Count,
+		resp.Meta.Page.Total)
+
+	fmt.Println()
+
+	// Print proxy-caches details
+	for _, proxy := range resp.Results {
+
+		fmt.Printf("ID: %s\n", proxy.ID)
+		fmt.Printf("Name: %s\n", proxy.Name)
+		fmt.Printf("Provider: %s\n", proxy.Provider)
+		fmt.Printf("URL: %s\n", proxy.URL)
+		fmt.Printf("Created at: %s\n", proxy.CreatedAt)
+		fmt.Printf("Updated at: %s\n", proxy.UpdatedAt)
+
+		fmt.Printf("-------------------------------------------\n")
+	}
+
+}
+
+func listAllProxyCaches(crClient *containerregistry.ContainerRegistryClient) {
+	// List proxy caches with pagination
+	resp, err := crClient.ProxyCaches().ListAll(context.Background(), containerregistry.ProxyCacheListAllOptions{
+		Sort: helpers.StrPtr("name:desc"),
+	})
+
+	if err != nil {
+		var httpError *client.HTTPError
+		if errors.As(err, &httpError) {
+			fmt.Printf("Status %s\n", httpError.Status)
+			fmt.Printf("Error body: %s\n", string(httpError.Body))
+		}
+
+		log.Fatal("Failed to list all proxy-caches")
+	}
+
+	fmt.Printf("All proxy-caches:\n\n")
+
+	// Print proxy-caches details
+	for _, proxy := range resp {
+		fmt.Printf("ID: %s\n", proxy.ID)
+		fmt.Printf("Name: %s\n", proxy.Name)
+		fmt.Printf("Provider: %s\n", proxy.Provider)
+		fmt.Printf("URL: %s\n", proxy.URL)
+		fmt.Printf("Created at: %s\n", proxy.CreatedAt)
+		fmt.Printf("Updated at: %s\n", proxy.UpdatedAt)
+
+		fmt.Printf("-------------------------------------------\n")
+	}
+}
+
+func createProxyCache(crClient *containerregistry.ContainerRegistryClient) string {
+	resp, err := crClient.ProxyCaches().Create(context.Background(), containerregistry.CreateProxyCacheRequest{
+		Name:        "test-proxy-cache",
+		Provider:    "docker-hub",
+		URL:         "https://hub.docker.com/repositories",
+		Description: helpers.StrPtr("Proxy-cache description"),
+	})
+
+	if err != nil {
+		var httpError *client.HTTPError
+		if errors.As(err, &httpError) {
+			fmt.Printf("Status %s\n", httpError.Status)
+			fmt.Printf("Error body: %s\n", string(httpError.Body))
+		}
+
+		log.Fatal("Failed to create a proxy-cache")
+	}
+
+	fmt.Printf("Created proxy-cache with ID: %s\n", resp.ID)
+
+	return resp.ID
+}
+
+func deleteProxyCache(crClient *containerregistry.ContainerRegistryClient, id string) {
+	err := crClient.ProxyCaches().Delete(context.Background(), id)
+
+	if err != nil {
+		var httpError *client.HTTPError
+		if errors.As(err, &httpError) {
+			fmt.Printf("Status %s\n", httpError.Status)
+			fmt.Printf("Error body: %s\n", string(httpError.Body))
+		}
+
+		log.Fatal("Failed to delete proxy-cache")
+	}
+
+	fmt.Printf("Deleted proxy-cache with ID: %s\n", id)
+}
+
+func updateProxyCache(crClient *containerregistry.ContainerRegistryClient, id string) {
+	resp, err := crClient.ProxyCaches().Update(context.Background(), id, containerregistry.UpdateProxyCacheRequest{
+		Name: helpers.StrPtr("new-test-proxy-cache"),
+	})
+
+	if err != nil {
+		var httpError *client.HTTPError
+		if errors.As(err, &httpError) {
+			fmt.Printf("Status %s\n", httpError.Status)
+			fmt.Printf("Error body: %s\n", string(httpError.Body))
+		}
+
+		log.Fatal("Failed to update proxy-cache")
+	}
+
+	fmt.Printf("Updated proxy-cache:\n")
+
+	fmt.Printf("ID: %s\n", resp.ID)
+	fmt.Printf("Name: %s\n", resp.Name)
+	fmt.Printf("Provider: %s\n", resp.Provider)
+	fmt.Printf("URL: %s\n", resp.URL)
+	fmt.Printf("Created at: %s\n", resp.CreatedAt)
+	fmt.Printf("Updated at: %s\n", resp.UpdatedAt)
+}
+
+func getProxyCache(crClient *containerregistry.ContainerRegistryClient, id string) {
+	resp, err := crClient.ProxyCaches().Get(context.Background(), id)
+
+	if err != nil {
+		var httpError *client.HTTPError
+		if errors.As(err, &httpError) {
+			fmt.Printf("Status %s\n", httpError.Status)
+			fmt.Printf("Error body: %s\n", string(httpError.Body))
+		}
+
+		log.Fatal("Failed to get a proxy-cache")
+	}
+
+	fmt.Printf("ID: %s\n", resp.ID)
+	fmt.Printf("Name: %s\n", resp.Name)
+	fmt.Printf("Description: %s\n", resp.Description)
+	fmt.Printf("Provider: %s\n", resp.Provider)
+	fmt.Printf("URL: %s\n", resp.URL)
+	fmt.Printf("Created at: %s\n", resp.CreatedAt)
+	fmt.Printf("Updated at: %s\n", resp.UpdatedAt)
 }
