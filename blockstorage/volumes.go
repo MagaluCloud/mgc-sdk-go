862afae982c11e2e1ac8cf8faf9f9bd0eb206b3d
NathalyaStefhany <nathalya.stefhany@hotmail.com>
2026-03-13 18:28:30 -0300

    fix: add omitempty

--- blockstorage/volumes.go
+++ blockstorage/volumes.go
@@ -78,12 +78,12 @@ type VolumeAttachment struct {
 
 // AttachmentInstance represents information about an instance attached to a volume.
 type AttachmentInstance struct {
-	ID        *string    `json:"id"`
-	Name      *string    `json:"name"`
-	Status    *string    `json:"status"`
-	State     *string    `json:"state"`
-	CreatedAt *time.Time `json:"created_at"`
-	UpdatedAt *time.Time `json:"updated_at"`
+	ID        *string    `json:"id,omitempty"`
+	Name      *string    `json:"name,omitempty"`
+	Status    *string    `json:"status,omitempty"`
+	State     *string    `json:"state,omitempty"`
+	CreatedAt *time.Time `json:"created_at,omitempty"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty"`
 }
 
 // CreateVolumeRequest represents the request to create a new volume.
