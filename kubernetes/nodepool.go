d800ab0e345807cc577b5dc07228b54c8f7b9fc5
Matheus Henrique Onofre de Oliveira <72235471+onofre-matheus@users.noreply.github.com>
2026-05-04 10:00:46 -0300

    undo k8s upgrade version (#174)
    * undo k8s upgrade version
    
    * retract version

--- kubernetes/nodepool.go
+++ kubernetes/nodepool.go
@@ -90,7 +90,6 @@ type (
 	PatchNodePoolRequest struct {
 		Replicas  *int       `json:"replicas,omitempty"`
 		AutoScale *AutoScale `json:"auto_scale,omitempty"`
-		Version   *string    `json:"version,omitempty"`
 	}
 
 	// nodePoolService implements the NodePoolService interface
